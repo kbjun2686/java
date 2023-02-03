2bcfb2b3a2d4ce9d391a3215c658e2f761e005d9
kbjun2686 <kbjun2686@gmail.com>
2023-02-03 14:22:37 +0900

    [feature] 대문자와 소문자

--- src/main/java/programmers/lv0/UppercaseLowercase.java
+++ src/main/java/programmers/lv0/UppercaseLowercase.java
@@ -1,15 +1,27 @@
 package programmers.lv0;
 
+
 // 대문자와 소문자
 public class UppercaseLowercase {
     public static void main(String[] args) {
-        System.out.println();
+        Solution result = new Solution();
+        System.out.println(result.solution("cccCCC"));
     }
+}
 
-    static class Solution {
-        public String solution(String my_string) {
-            String answer = "";
-            return answer;
+class Solution {
+    public String solution(String my_string) {
+        char[] arr = my_string.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (65 <= arr[i] && arr[i] <= 90) {
+                arr[i] = (char) (arr[i] + 32);
+                continue;
+            }
+            if (97 <= arr[i] && arr[i] <= 122) {
+                arr[i] = (char) (arr[i] - 32);
+            }
         }
+        return String.valueOf(arr);
     }
 }
