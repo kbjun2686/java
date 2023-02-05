0fe7595962761a3f1939ee595e08bc7da7d0239b
kbjun2686 <kbjun2686@gmail.com>
2023-02-05 17:18:45 +0900

    [feature] 369게임

--- src/main/java/programmers/lv0/ThreeSixNine.java
+++ src/main/java/programmers/lv0/ThreeSixNine.java
@@ -21,3 +21,19 @@ public class ThreeSixNine {
         }
     }
 }
+
+// 다른 풀이
+//class Solution {
+//    public int solution(int order) {
+//        int answer = 0;
+//
+//        String str = order+"";
+//
+//        for(int i=0; i<str.length(); i++){
+//            char c = str.charAt(i);
+//            if(c=='3'||c=='6'||c=='9') answer++;
+//        }
+//
+//        return answer;
+//    }
+//}
